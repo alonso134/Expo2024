97c9f9d379fd66a401c0c3db85266862cab30dd9
hwrnadez <20220664@ricaldone.edu.sv>
2024-06-27 11:13:07 -0600

    funcionalidad de index

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,75 +1,61 @@
-(() => {
-    'use strict'
-  
-    const getStoredTheme = () => localStorage.getItem('theme')
-    const setStoredTheme = theme => localStorage.setItem('theme', theme)
-  
-    const getPreferredTheme = () => {
-      const storedTheme = getStoredTheme()
-      if (storedTheme) {
-        return storedTheme
-      }
-  
-      return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+// Constante para establecer el formulario de registro del primer usuario.
+const SIGNUP_FORM = document.getElementById('signupForm');
+// Constante para establecer el formulario de inicio de sesión.
+const LOGIN_FORM = document.getElementById('loginForm');
+
+// Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', async () => {
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+    loadTemplate();
+    // Petición para consultar los usuarios registrados.
+    const DATA = await fetchData(USER_API, 'readUsers');
+    // Se comprueba si existe una sesión, de lo contrario se sigue con el flujo normal.
+    if (DATA.session) {
+        // Se direcciona a la página web de bienvenida.
+        location.href = 'inicio.html';
+    } else if (DATA.status) {
+        // Se establece el título del contenido principal.
+        MAIN_TITLE.textContent = 'Iniciar sesión';
+        // Se muestra el formulario para iniciar sesión.
+        LOGIN_FORM.classList.remove('d-none');
+        sweetAlert(4, DATA.message, true);
+    } else {
+        // Se establece el título del contenido principal.
+        MAIN_TITLE.textContent = 'Registrar primer usuario';
+        // Se muestra el formulario para registrar el primer usuario.
+        SIGNUP_FORM.classList.remove('d-none');
+        sweetAlert(4, DATA.error, true);
     }
-  
-    const setTheme = theme => {
-      if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.documentElement.setAttribute('data-bs-theme', 'dark')
-      } else {
-        document.documentElement.setAttribute('data-bs-theme', theme)
-      }
+});
+
+// Método del evento para cuando se envía el formulario de registro del primer usuario.
+SIGNUP_FORM.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SIGNUP_FORM);
+    // Petición para registrar el primer usuario del sitio privado.
+    const DATA = await fetchData(USER_API, 'signUp', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        sweetAlert(1, DATA.message, true, 'index.html');
+    } else {
+        sweetAlert(2, DATA.error, false);
     }
-  
-    setTheme(getPreferredTheme())
-  
-    const showActiveTheme = (theme, focus = false) => {
-      const themeSwitcher = document.querySelector('#bd-theme')
-  
-      if (!themeSwitcher) {
-        return
-      }
-  
-      const themeSwitcherText = document.querySelector('#bd-theme-text')
-      const activeThemeIcon = document.querySelector('.theme-icon-active use')
-      const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-      const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
-  
-      document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-        element.classList.remove('active')
-        element.setAttribute('aria-pressed', 'false')
-      })
-  
-      btnToActive.classList.add('active')
-      btnToActive.setAttribute('aria-pressed', 'true')
-      activeThemeIcon.setAttribute('href', svgOfActiveBtn)
-      const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
-      themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
-  
-      if (focus) {
-        themeSwitcher.focus()
-      }
+});
+
+// Método del evento para cuando se envía el formulario de inicio de sesión.
+LOGIN_FORM.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(LOGIN_FORM);
+    // Petición para iniciar sesión.
+    const DATA = await fetchData(USER_API, 'logIn', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        sweetAlert(1, DATA.message, true, 'inicio.html');
+    } else {
+        sweetAlert(2, DATA.error, false);
     }
-  
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-      const storedTheme = getStoredTheme()
-      if (storedTheme !== 'light' && storedTheme !== 'dark') {
-        setTheme(getPreferredTheme())
-      }
-    })
-  
-    window.addEventListener('DOMContentLoaded', () => {
-      showActiveTheme(getPreferredTheme())
-  
-      document.querySelectorAll('[data-bs-theme-value]')
-        .forEach(toggle => {
-          toggle.addEventListener('click', () => {
-            const theme = toggle.getAttribute('data-bs-theme-value')
-            setStoredTheme(theme)
-            setTheme(theme)
-            showActiveTheme(theme, true)
-          })
-        })
-    })
-  })()
-  
+});
