6ae03f1b32883d879d620ce5bad1ecc044e351ce
Alonso Moreno <20220664@ricaldone.edu.sv>
2024-06-29 00:12:40 -0600

    inicio echo

--- controllers/utils/admin.js
+++ controllers/utils/admin.js
@@ -34,13 +34,13 @@ const loadTemplate = async () => {
                 <nav class="navbar bg-body-tertiary fixed-top ">
                   <div class="container-fluid ">
                
-                    <a class="navbar-brand"><img src="../../resources/img/logo.png" alt="Logo"></a>
+                    <a class="navbar-brand"><img src="../../resources/img/CECSL.png" alt="Logo"></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                       <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Administrador</h5>
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Profesor</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                       </div>
                       <div class="offcanvas-body">
@@ -102,7 +102,7 @@ const loadTemplate = async () => {
                 <header>
                     <nav class="navbar fixed-top bg-body-tertiary">
                         <div class="container">
-                            <a class="navbar-brand" href="index.html">
+                            <a class="navbar-brand" href="CECSL.html">
                                 <img src="../../resources/img/logo.png" alt="inventory" width="50">
                             </a>
                         </div>
